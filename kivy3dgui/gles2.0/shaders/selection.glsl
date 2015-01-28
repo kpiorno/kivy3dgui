2677e50a1c3c1de7def8c4101b7d11b07e7293f6
Karel Piorno Charchabal <kpiorno@uci.cu>
2015-01-28 14:42:59 +0100

    Remove comments

--- kivy3dgui/gles2.0/shaders/selection.glsl
+++ kivy3dgui/gles2.0/shaders/selection.glsl
@@ -1,9 +1,3 @@
-/* simple.glsl
-
-simple diffuse lighting based on laberts cosine law; see e.g.:
-    http://en.wikipedia.org/wiki/Lambertian_reflectance
-    http://en.wikipedia.org/wiki/Lambert%27s_cosine_law
-*/
 ---VERTEX SHADER-------------------------------------------------------
 #ifdef GL_ES
     precision highp float;
